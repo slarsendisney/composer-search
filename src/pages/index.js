611324396fa27d5edb498ac0f4b3ea04f8b24261
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-03-19 19:58:46 +0000

    feat(input): added search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { graphql } from "gatsby";
 
 const Home = ({ data }) => {
+  const [filter, setFilter] = useState("");
   const {
     allResultsJson: { nodes },
     siteBuildMetadata: { buildTime },
   } = data;
-  console.log(nodes);
-  const totalComposers = nodes.reduce((acc, cur) => {
-    acc += cur.composers ? cur.composers.length : 0;
-    return acc
-  }, 0);
+  const totalComposers = useMemo(
+    () =>
+      nodes.reduce((acc, cur) => {
+        acc += cur.composers ? cur.composers.length : 0;
+        return acc;
+      }, 0),
+    [nodes]
+  );
+  const results = useMemo(() => {
+    return filter !== ""
+      ? nodes.filter(({ composers, website }) => {
+          const inComposers = composers
+            ? composers.filter((name) => name.includes(filter)).length !== 0
+            : false;
+          const inWebsite = website.includes(filter);
+          return inComposers || inWebsite;
+        })
+      : nodes;
+  }, [filter, nodes]);
+
   return (
     <div className="max-w-4xl text-gray-800 mx-auto p-4 md:py-8">
       <div className="flex-col">
@@ -18,13 +34,37 @@ const Home = ({ data }) => {
           Composer Search
         </h1>
         <p className="md:my-3">
-          👋 I found {totalComposers} matching composer names across {nodes.length} sites. I
-          last checked on{" "}
+          👋 I found {totalComposers} matching composer names across{" "}
+          {nodes.length} sites. I last checked on{" "}
           <span className="font-bold">
             {new Date(buildTime).toDateString()}
           </span>
           .
         </p>
+        <div className="relative group">
+          <input
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="w-64 mb-2 border-2 border-gray-300 bg-white h-10 px-5 pl-8 rounded-lg text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search by site or composer"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="absolute top-0 left-0 m-3 h-4 w-4 text-gray-600 "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
         <div className="hidden md:grid grid-cols-2 gap-4 border-b-2 pb-4 opacity-75">
           <div className="flex items-center">
             <svg
@@ -61,9 +101,14 @@ const Home = ({ data }) => {
             <p>COMPOSERS</p>
           </div>
         </div>
-        {nodes.map(({ composers, website, error }) => (
+        {results.map(({ composers, website, error }) => (
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-800 border-b-2 py-4">
-            <a href={website} className="text-blue-600 hover:underline mt-3">
+            <a
+              href={website}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline mt-3"
+            >
               {website.replace(/(^\w+:|^)\/\//, "")}
             </a>
             {error ? (
