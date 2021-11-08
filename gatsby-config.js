86649c947581516324bc6b8fb4a0440192f1dadd
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-11-08 10:45:42 +0000

    added gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-buildtime-timezone`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-plugin-sass`,
       options: {
